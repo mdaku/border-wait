ba2e4de940bc0a6a272415555bc96730f43ffc96
Sean Nossek <sean.nossek@mail.mcgill.ca>
2019-06-21 12:03:58 -0400

    init

--- border-wait.R
+++ border-wait.R
@@ -8,5 +8,33 @@
 
 library(tidyverse)
 library(jsonlite)
+library(readr)
+library(twitteR)
 
 # Let's go
+
+######## Read .csv and format for tweet - a visual?
+
+# Read .csv - find way to execute this every hour? Do we need a server?
+
+current_waits <- read_delim("http://www.cbsa-asfc.gc.ca/bwt-taf/bwt-eng.csv", delim = ";") %>% select(1,3,5,7,9,11)
+
+# make visual or string for tweeting?
+
+
+
+
+
+
+######## Tweet
+
+# fill in info
+setup_twitter_oauth(consumer_key = "",
+                    access_token = "",
+                    consumer_secret = "",
+                    access_secret = "")
+
+# tweet!  
+tw <- updateStatus("My first tweet from R!",mediaPath = "example.jpg")
+
+########
