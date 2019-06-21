e0156cf3a69507cabd1bb62451db662b1d198903
Mark Daku <markdaku@Marks-MacBook-Air.local>
2019-06-21 11:17:20 -0400

    First commit

---
+++ border-wait.R
@@ -0,0 +1,12 @@
+# border-wait.R
+
+
+# @author Mark Daku
+# @author Sean Nossek
+# @date June 21, 2019
+# @description Simple twitter application to tweet out border wait times based on open Canadian data
+
+library(tidyverse)
+library(jsonlite)
+
+# Let's go
